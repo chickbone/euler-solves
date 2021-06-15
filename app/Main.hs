1b76631a0952c529e647b283278c81752ede0385
Kasshi_K <kasshi.4259.b.b@gmail.com>
2021-06-15 22:07:21 +0900

    Passed Problem 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
+import Control.Applicative
+import Control.Monad
+import Data.Maybe
+import Data.Monoid
 import Lib
 
 main :: IO ()
-main = someFunc
+main = print $ sum $ ans1 <$> [1 .. 999]
+
+ans1 :: Int -> Int
+ans1 max = fromMaybe 0 $ max <$ guard (max `mod` 3 == 0 || max `mod` 5 == 0)
