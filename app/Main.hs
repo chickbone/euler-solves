20e8f68c35198786956455fd9163539ef9039b4e
Kasshi_K <kasshi.4259.b.b@gmail.com>
2021-06-26 16:30:56 +0900

    Add ans14

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,29 @@ module Main where
 
 import Control.Applicative
 import Control.Monad
+import Data.Bits
+import Data.Function
 import Data.Maybe
 import Data.Monoid
-import Lib
 
 main :: IO ()
-main = print $ sum $ ans1 <$> [1 .. 999]
+main = print $ ans14 1
 
 ans1 :: Int -> Int
 ans1 max = fromMaybe 0 $ max <$ guard (max `mod` 3 == 0 || max `mod` 5 == 0)
+
+ans2 :: Int -> Int
+ans2 x = undefined
+
+ans5 x = undefined
+
+ans14 :: Int -> Int
+ans14 x = fix trans
+  where
+    trans :: Int -> Int
+    trans n
+      | n == 1 = 1
+      | n .&. 1 == 0 = n `shiftL` 1
+      | n .&. 1 == 1 = 3 * n + 1
+
+fib a b = a : b : zipWith (+) (fib a b) (tail $ fib a b)
